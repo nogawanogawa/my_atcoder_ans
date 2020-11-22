df4c36d69d99afd71842405eb6cf02bc804b6a4e
nogawanogawa <t_tsuno_tsuno@yahoo.co.jp>
2020-11-23 00:35:08 +0900

    [fix] 184 c

--- abc184/c.go
+++ abc184/c.go
@@ -10,13 +10,16 @@ func main() {
     fmt.Scanf("%d %d", &r1, &c1)
     fmt.Scanf("%d %d", &r2, &c2)
 	
+	r := r1 - r2
+	c := c1 - c2
+
 	var ans int 
 
 	if (r1 == r2) && (c1 == c2){
 		ans = 0		
 	} else if (r1+c1 == r2+c2) || (r1-c1 == r2-c2) || math.Abs(float64(r1-r2)) + math.Abs(float64(c1-c2)) <=3{
 		ans = 1
-  	} else if ((r2 - r1 + c2 - c1) % 2 == 0) || (math.Abs(float64(r2 - r1 - c2 + c1)) <= 3) || (math.Abs(float64(r2 - r1 + c2 - c1)) <= 3){
+  	} else if ((r2 - r1 + c2 - c1) % 2 == 0) || (math.Abs(float64(r2 - r1 - c2 + c1)) <= 3) || (math.Abs(float64(r2 - r1 + c2 - c1)) <= 3) || (math.Abs(float64(r)) + math.Abs(float64(c)) <=6){
 		ans = 2
 	} else {
 		ans = 3
